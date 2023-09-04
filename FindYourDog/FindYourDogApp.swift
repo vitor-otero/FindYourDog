599ee8035b8b7c3fde0126475d13cc02bde74e8e
Vitor Otero <votero.ext@gmv.com>
2023-09-04 19:23:51 +0100

    Initial Commit

---
+++ FindYourDog/FindYourDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindYourDogApp.swift
+//  FindYourDog
+//
+//  Created by Vítor Otero on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct FindYourDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
