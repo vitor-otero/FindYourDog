8910cc3ba0e12227fe0da1fbc9ceeb468ed4ff9c
Vitor Otero <votero.ext@gmv.com>
2023-09-04 21:13:19 +0100

    Add a Breed realm model to save the image to download only once.

---
+++ FindYourDog/Components/BreedRealm.swift
@@ -0,0 +1,21 @@
+//
+//  BreedRealm.swift
+//  FindYourDog
+//
+//  Created by Vítor Otero on 04/09/2023.
+//
+
+import Foundation
+import RealmSwift
+
+class BreedRealm: Object {
+    @objc dynamic var id = ""
+    @objc dynamic var name = ""
+    @objc dynamic var referenceImageId = ""
+    @objc dynamic var imageData: Data?
+
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
