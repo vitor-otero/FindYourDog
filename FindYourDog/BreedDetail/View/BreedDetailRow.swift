a02c48c92187cbb38f8bbc3ae320104f1ce7dd38
Vitor Otero <votero.ext@gmv.com>
2023-09-06 04:49:29 +0100

    Add DetailView and VM

---
+++ FindYourDog/BreedDetail/View/BreedDetailRow.swift
@@ -0,0 +1,27 @@
+//
+//  BreedDetailRow.swift
+//  FindYourDog
+//
+//  Created by Vítor Otero on 04/09/2023.
+//
+
+import SwiftUI
+
+struct BreedDetailRow: View {
+    let title: String
+    let detail: String
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(.blue)
+            Spacer()
+            Text(detail)
+                .font(.subheadline)
+        }
+        .padding(.horizontal)
+    }
+}
+
